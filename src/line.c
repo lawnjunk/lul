23bd640b8e67f51014f8244e05803dad28c1fb38
Duncan Marsh <dwolfm@gmail.com>
2017-01-22 21:52:34 -0800

    started refactoring line

--- src/line.c
+++ src/line.c
@@ -18,27 +18,17 @@ char *line_to_string(line_t *line){
   return result;
 }
 
-line_t *line_add_ch(line_t *line, char c){
-  line->buffer[line->length++] = c;
+line_t *line_append_ch(line_t *line, char c){
+  if(line->length == line->size) {
+    err_trouble_on(line->err, "not enough room");
+    return line;
+  }
+  buffer_write_char(line->buffer, c, line->length++);
   return line;
 }
 
-line_t *line_write(line_t *line, char *input){
-  char *text = line->buffer;
-  strcat(text, input);
-  line->length = strlen(text);
-  return line;
-}
+line_t *line_insert_ch(line_t *line, char ch, int offset){
 
-line_t *line_insert(line_t *line, int offset, char *input){
-  char *result = (char *) malloc(sizeof(char) * LINE_WITH);
-  strncpy(result, line->buffer, offset);
-  strcat(result, input);
-  strcat(result, (line->buffer) + offset);
-  free(line->buffer);
-  line->buffer = result;
-  line->length = strlen(line->buffer);
-  return line;
 };
 
 
@@ -48,40 +38,15 @@ line_t *line_insert(line_t *line, int offset, char *input){
 //   o  c
 //   
 
-line_t *line_remove(line_t *line, int offset, int count){
-  char *result = (char *) malloc(sizeof(char) * LINE_WITH);
-  if (offset > line->length) offset = line->length; 
-  strncpy(result, line->buffer, offset);  // copy upto the offset
-  int end = offset + count;
-  if (end < line->size)
-    strcat(result, line->buffer + end); // copy from end on 
-  free(line->buffer); // free the old line
-  line->buffer = result;
-  line->length = strlen(line->buffer);
-  return line;
-}
-
-line_t *line_replace(line_t *line, int offset, char *input){
-  char *result = (char *) malloc(sizeof(char) * LINE_WITH);
-  int input_lenth = strlen(input);
-  strncpy(result, line->buffer, offset);
-  strcat(result, input);
-  strcat(result, (line->buffer) + input_lenth + offset);
-  line->buffer = result;
-  line->length = strlen(line->buffer);
-  return line;
-};
-
 line_t *line_create(char *text){
   line_t *result = (line_t *) malloc(sizeof(line_t));  
   result->buffer = buffer_create(LINE_WITH);
-  buffer_write(text);
-  result->length = strlen(text);;
+  int i=0;
+  while(text[i] != '\0'){
+    buffer_write_char(result->buffer, text[i], i);
+    i++;
+  }
+  result->length = strlen(i);
   result->size = LINE_WITH;
   return result;
-}
-
-line_t *line_from_string(char *text){
-  line_t *result = line_new();
-  return line_write(line, text);
 }
--- src/line.h
+++ src/line.h
@@ -20,8 +20,8 @@ line_t *line_free(line_t *line);
 bool line_is_evil(line_t *line);
 char *line_to_string(line_t *line);
 char line_read_ch(line_t *line, int offset);
-line_t * line_append_ch(line_t *line, char c);
+line_t * line_append_ch(line_t *line, char ch);
 line_t * line_delete_ch(line_t *line, int offset);
-line_t * line_insert_ch(line_t *line, int offset, char c);
+line_t * line_insert_ch(line_t *line, char ch, int offset);
 
 #endif
